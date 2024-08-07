b608d3cc7492b9e6a7d7e24de2d480466ee4a23f
cmolina <ciromolina86@gmail.com>
2024-08-06 20:18:30 -0400

    organize code and functions

--- utils/main_tag_list.py
+++ utils/main_tag_list.py
@@ -21,60 +21,78 @@ def get_tag_type(file_path, tag_type='struct'):
     _tags_df2 = _tags_df.transpose()
 
     if tag_type == 'atomic':
-        _atomic_tags = _tags_df2.where(_tags_df2['tag_type'] == 'atomic')
-        return _atomic_tags.dropna(how='all')  # .to_excel('tags_atomic.xlsx')
+        tags_df = _tags_df2.where(_tags_df2['tag_type'] == 'atomic')
+        return tags_df.dropna(how='all')  # .to_excel('tags_atomic.xlsx')
     if tag_type == 'struct':
-        _struct_tags = _tags_df2.where(_tags_df2['tag_type'] == 'struct')
-        return _struct_tags.dropna(how='all')  # .to_excel('tags_struct.xlsx')
+        tags_df = _tags_df2.where(_tags_df2['tag_type'] == 'struct')
+        return tags_df.dropna(how='all')  # .to_excel('tags_struct.xlsx')
 
 
 def get_data_type(tags_df, data_type='P_AIn'):
     if data_type == 'P_AIn':
         tags_df_filtered = tags_df.where(tags_df['data_type_name'] == 'P_AIn')
+        return tags_df_filtered.dropna(how='all')  # ['tag_name', 'data_type_name', 'data_type']
+    if data_type == 'P_Motor':
+        tags_df_filtered = tags_df.where(tags_df['data_type_name'] == 'P_Motor')
+        return tags_df_filtered.dropna(how='all')  # ['tag_name', 'data_type_name', 'data_type']
 
-    _p_ain_tags_short = tags_df_filtered[['tag_name', 'data_type_name', 'data_type']].dropna(how='all')
 
-    new_df = pd.DataFrame(_p_ain_tags_short['tag_name'])
+def insert_attributes(tags_df_filtered):
+    new_df = pd.DataFrame(tags_df_filtered['tag_name'])
 
-    for index, row in _p_ain_tags_short.iterrows():
+    for index, row in tags_df_filtered.iterrows():
         temp_dict = {}
         temp_dict.update({'attributes': get_attributes(row)})
         temp_df = pd.DataFrame(temp_dict)
         new_df = new_df.merge(temp_df, how='cross')
         break
 
-    _p_ain_tags_short = _p_ain_tags_short[['tag_name', 'data_type_name']]
-    print(_p_ain_tags_short.shape)
-    _p_ain_tags_short = _p_ain_tags_short.merge(new_df, on='tag_name', how='left')
-    print(_p_ain_tags_short.shape)
-    # _p_ain_tags_short.to_excel('tags_P_AIn.xlsx')
+    tags_df_filtered = tags_df_filtered[['tag_name', 'data_type_name']]
+    print(tags_df_filtered.shape)
+    tags_df_filtered = tags_df_filtered.merge(new_df, on='tag_name', how='left')
+    print(tags_df_filtered.shape)
 
-    return _p_ain_tags_short
+    return tags_df_filtered
 
 
-def get_tag_list():
-    pass
-    # tag_list = f"tag_list-{time.strftime('%Y-%m-%d-%H-%M')}.txt"
+def get_tag_list(clx):
+    # Read tags from PLC
+    data = clx.get_tag_list_json()
 
-    # with open(tag_list, mode='w', newline='') as file:
-    #     file.write(str(clx.get_tag_list_json()))
+    # Writing tag list in JSON format
+    with open(f"tag-list-{time.strftime('%Y-%m-%d')}.json", 'w') as file:
+        json.dump(data, file)
+
+    return json.dumps(data)
+
+
+def get_plc_info(clx):
+    # Read PLC info
+    data = clx.get_plc_info()
+
+    # Writing PLC info in JSON format
+    with open(f"plc-info-{time.strftime('%Y-%m-%d')}.json", 'w') as file:
+        json.dump(data, file)
+
+    return json.dumps(data)
 
 
 def main():
-    # Create PLC object and print PLC info
+    # Create PLC object
     clx = comm_handler.CLX_Manager(ip_address='192.168.60.80')
-    print(clx.get_plc_info())
 
-    # read tag list from PLC
-    # tag_list_json = json.loads(str(clx.get_tag_list_json()).replace("'", '"'))
-    tag_list_data = json.dumps(clx.get_tag_list_json())
+    # Read data from PLC and write to json files
+    get_plc_info(clx)
+    get_tag_list(clx)
 
-    # Writing tag list in JSON format
-    with open(f"tag-list-{time.strftime('%Y-%m-%d-%H-%M')}.json", 'w') as file:
-        json.dump(tag_list_data, file)
+    # Parse tag list read from PLC and filter data type of interest
+    tag_list_atomic = get_tag_type(file_path='tag-list-2024-08-07.json', tag_type='atomic')
 
-    structs = get_structs()
-    p_ains = get_p_ain(structs)
+    # Parse tag list read from PLC and filter data type of interest
+    tag_list_struct = get_tag_type(file_path='tag-list-2024-08-07.json', tag_type='struct')
+    tag_list_p_ain = get_data_type(tags_df=tag_list_struct, data_type='P_AIn')
+    tag_list_p_ain_with_attributes = insert_attributes(tag_list_p_ain)
+    tag_list_p_ain_with_attributes.to_excel('tag_list_p_ain_with_attributes.xlsx')
 
 
 if __name__ == '__main__':
