383c62fcd5bc138558d66da8467b6e381f1bd979
Ciro Molina <cmolina@itgtec.com>
2024-08-09 15:59:02 -0400

    p_ain_helper was added. filter_setpoints needs to be implemented to leave customer attributes only.

--- utils/main_helper.py
+++ utils/main_helper.py
@@ -8,7 +8,7 @@ from handlers import comm_handler, signal_handler
 import json
 
 
-def get_instances(file_path, data_type='P_AIn'):
+def get_instances(file_path='tag-list-2024-08-09.json', data_type='P_AIn'):
     instances = []
     # open JSON file and load instances into a dictionary
     with open(file_path, 'r') as file:
@@ -21,16 +21,16 @@ def get_instances(file_path, data_type='P_AIn'):
     return instances
 
 
-def get_attributes(file_path, data_type='P_AIn'):
+def get_attributes(file_path='attributes_setpoints.json', data_type='P_AIn'):
     # open JSON file and load attributes into a dictionary
     with open(file_path, 'r') as file:
         data = json.load(file)
 
     # filter attributes by data type
-    return data[data_type]['attributes'][0] + data[data_type]['attributes'][1]
+    return data[data_type]['attributes']
 
 
-def get_std_cfg_values(file_path, data_type='P_AIn'):
+def get_common_values(file_path='attributes_setpoints_common_values.json', data_type='P_AIn'):
     # open JSON file and load attributes into a dictionary
     with open(file_path, 'r') as file:
         data = json.load(file)
@@ -45,6 +45,7 @@ def read_setpoints(plc, instances, attributes):
     data = {}
 
     for instance in instances:
+        print(instance)
         # Create a dictionary for the current instance
         instance_dict = {}
         values = plc.read_multiple_tags([f'{instance}.{attribute}' for attribute in attributes])
@@ -57,19 +58,19 @@ def read_setpoints(plc, instances, attributes):
         data[instance] = instance_dict
 
         # Writing instance attributes in JSON format
-        with open(f"setpoints-{time.strftime('%Y-%m-%d')}.json", 'w') as file:
+        with open(f"setpoints-1-{time.strftime('%Y-%m-%d')}.json", 'w') as file:
             json.dump(data, file, indent=4)
 
-        time.sleep(3)
+        time.sleep(1)
 
     return data
 
 
 def write_setpoints(plc, instances, attributes, values):
     for instance in instances:
+        print(instance)
         plc.write_multiple_tags([f'{instance}.{attribute}' for attribute in attributes], values)
-
-        time.sleep(3)
+        time.sleep(1)
 
 
 def export_setpoints(file_path):
@@ -84,21 +85,60 @@ def export_setpoints(file_path):
     df.to_excel(f"setpoints-{time.strftime('%Y-%m-%d')}.xlsx", index=True, engine='openpyxl')
 
 
+def filter_setpoints(file_path, filter_file_path):
+    # todo
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    attributes = list(data[data_type]['attributes'].keys())
+    values = [data[data_type]['attributes'][attribute] for attribute in attributes]
+
+    return attributes, values
+
+
+def p_ain_helper(clx):
+    # get a list of all the P_AIn instances
+    instances = get_instances('tag-list-2024-08-09.json', data_type='P_AIn')
+
+    if False:  # if you want writing functions to run manually change to True, if not change to False
+        # get a list of all the attributes and values for the common values that do not change between instances
+        attributes, values = get_common_values('attributes_setpoints_common_values.json', 'P_AIn')
+        attributes_alarm, values_alarm = get_common_values('attributes_setpoints_common_values.json', 'P_Alarm')
+
+        # adding the P_Alarm setpoint attributes
+        attributes_alarm = [f"{alarm}.{attribute}" for alarm in ['HiHi', 'Hi', 'Lo', 'LoLo', 'Fail']
+                            for attribute in attributes_alarm]
+        attributes = attributes + attributes_alarm
+        values = values + values_alarm*5
+
+        # write all the common setpoint attributes to all the P_AIn instances
+        write_setpoints(clx, instances, attributes, values)
+        # if it fails before finishing, use the instance where it failed as a starting point to continue
+        # write_setpoints(clx, instances[instances.index('FIT_40_14'):], attributes, values)
+
+    if True:  # if you want reading functions to run manually change to True, if not change to False
+        # get a list of all the setpoint attributes for P_Ain instances
+        attributes = get_attributes('attributes_setpoints.json', data_type='P_AIn')
+
+        # adding the P_Alarm setpoint attributes
+        attributes_alarm = [f"{alarm}.{attribute}" for alarm in ['HiHi', 'Hi', 'Lo', 'LoLo', 'Fail']
+                            for attribute in get_attributes('attributes_setpoints.json', data_type='P_Alarm')]
+        attributes = attributes + attributes_alarm
+
+        # read all the setpoint attributes to all the P_AIn instances
+        read_setpoints(clx, instances[:], attributes)
+        # if it fails before finishing, use the instance where it failed as a starting point to continue
+        # read_setpoints(clx, instances[instances.index('AIT_70_31H'):], attributes)
+
+        # if you want to export json as an excel file uncomment this line
+        # export_setpoints('setpoints-2024-08-07.json')
+
+
+
 def main():
     # Create PLC object
     clx = comm_handler.CLX_Manager(ip_address='192.168.60.80')
-
-    instances = get_instances('tag-list-2024-08-08.json', data_type='P_AIn')
-    # attributes = get_attributes('attributes_setpoints.json', data_type='P_AIn')
-    # read_setpoints(clx, instances[:3], attributes)
-    # print(instances)
-    # print(len(instances))
-    # print(instances.index('PIT_40_11B'))
-
-    # export_setpoints('setpoints-2024-08-07.json')
-
-    attributes, values = get_std_cfg_values('attributes_setpoints_common_values.json')
-    write_setpoints(clx, instances, attributes, values)
+    p_ain_helper(clx)
 
 
 if __name__ == '__main__':
