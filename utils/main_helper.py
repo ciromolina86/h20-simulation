dd6f4e39492acdf0cb0624517c4b8c445093d666
Ciro Molina <cmolina@itgtec.com>
2024-08-28 17:06:38 -0400

    added scada_tags method to generate csv files to import instances in iFix

--- utils/main_helper.py
+++ utils/main_helper.py
@@ -137,13 +137,15 @@ def p_ain_helper(clx):
         # if you want to export json as an excel file uncomment this line
         # export_setpoints('setpoints-2024-08-07.json')
 
-    if True:  # if you want attributes filtering functions to run manually change to True, if not change to False
+    if False:  # if you want attributes filtering functions to run manually change to True, if not change to False
         custom_attributes = get_attributes('attributes_setpoints.json', data_type='P_AIn', group='custom_attributes')
         custom_attributes = custom_attributes + \
                             [f"{alarm}.{attribute}" for alarm in ['HiHi', 'Hi', 'Lo', 'LoLo', 'Fail']
                              for attribute in get_attributes('attributes_setpoints.json', data_type='P_Alarm',
                                                              group='custom_attributes')]
         filter_setpoints('setpoints-3-2024-08-09.json', custom_attributes)
+
+    return instances
 
 
 def get_data_types(file_path='tag-list-2024-08-09.json'):
@@ -170,11 +172,86 @@ def get_data_types(file_path='tag-list-2024-08-09.json'):
     return data_types
 
 
+def scada_tags():
+    instances = get_instances(data_type='P_ValveSO')
+    # print(instances)
+    df = pd.read_csv('database TagSourceHeader.csv')
+    # print(df)
+
+    '''#AssetHeader,Asset Name,Description,Asset Type Name,Parent Asset Name,Asset Type Template,'''
+    # for instance in instances:
+    #     new_row = df.loc[5]
+    #     new_row['Asset Name'] = instance
+    #     df.loc[len(df)] = new_row
+    # df.to_csv('AssetHeader P_Motor.csv', index=False)
+
+    '''#AssetSubstituteParameters,ParameterName,AssetName,AssetTemplate,AssetType,ParamterValue,'''
+    # for instance in instances:
+    #     new_row = df.loc[2]
+    #     new_row['AssetName'] = new_row['ParamterValue'] = instance
+    #     df.loc[len(df)] = new_row
+    # df.to_csv('AssetSubstituteParameters P_ValveSO.csv', index=False)
+
+    '''#TagSourceHeader,Parent Asset Name,Property Name,RealtimeServerAlias,RealtimeDataSourceName (iFix tag Id),HistoricalServerAlias,HistoricalDataSourceName,'''
+    # for instance in instances:
+    #     rows_to_copy = df[df['Parent Asset Name'] == 'Test_P_ValveSO'].copy()
+    #     rows_to_copy['Parent Asset Name'] = rows_to_copy['Parent Asset Name'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
+    #     rows_to_copy['RealtimeDataSourceName (iFix tag Id)'] = rows_to_copy['RealtimeDataSourceName (iFix tag Id)'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
+    #     rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(lambda x: str(x))
+    #     rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
+    #     df = pd.concat([df, rows_to_copy], ignore_index=True)
+    #
+    # df.to_csv('TagSourceHeader P_ValveSO.csv', index=False)
+
+    # print(df.shape)
+
+
 def main():
     # Create PLC object
-    clx = comm_handler.CLX_Manager(ip_address='192.168.60.80')
-    get_data_types(file_path='tag-list-2024-08-09.json')
+    # clx = comm_handler.CLX_Manager(ip_address='192.168.60.80')
+    # get_data_types(file_path='tag-list-2024-08-09.json')
     # p_ain_helper(clx)
+
+    with open('tag-list-2024-08-09.json', 'r') as file:
+        data = json.load(file)
+
+    # df = pd.DataFrame({"attributes": data['LIT_15_11']['data_type']['attributes'],
+    #                    "Faceplate": ['' for x in data['LIT_15_11']['data_type']['attributes']],
+    #                    "Alarm": ['' for x in data['LIT_15_11']['data_type']['attributes']],
+    #                    "Trend": ['' for x in data['LIT_15_11']['data_type']['attributes']]})
+    # print(df.head(5))
+    # df.to_excel('P_AIn attributes.xlsx')
+
+    # df = pd.DataFrame({"attributes": data['PMP_15_11']['data_type']['attributes'],
+    #                    "Faceplate": ['' for x in data['PMP_15_11']['data_type']['attributes']],
+    #                    "Alarm": ['' for x in data['PMP_15_11']['data_type']['attributes']],
+    #                    "Trend": ['' for x in data['PMP_15_11']['data_type']['attributes']]})
+    # print(df.head(5))
+    # df.to_excel('P_VSD attributes.xlsx')
+
+    # df = pd.DataFrame({"attributes": data['CMP_30_61']['data_type']['attributes'],
+    #                    "Faceplate": ['' for x in data['CMP_30_61']['data_type']['attributes']],
+    #                    "Alarm": ['' for x in data['CMP_30_61']['data_type']['attributes']],
+    #                    "Trend": ['' for x in data['CMP_30_61']['data_type']['attributes']]})
+    # print(df.head(5))
+    # df.to_excel('P_Motor attributes.xlsx')
+
+    # df = pd.DataFrame({"attributes": data['VBF_70_27']['data_type']['attributes'],
+    #                    "Faceplate": ['' for x in data['VBF_70_27']['data_type']['attributes']],
+    #                    "Alarm": ['' for x in data['VBF_70_27']['data_type']['attributes']],
+    #                    "Trend": ['' for x in data['VBF_70_27']['data_type']['attributes']]})
+    # print(df.head(5))
+    # df.to_excel('P_ValveC attributes.xlsx')
+
+    # df = pd.DataFrame({"attributes": data['FC_70_51']['data_type']['attributes'],
+    #                    "Faceplate": ['' for x in data['FC_70_51']['data_type']['attributes']],
+    #                    "Alarm": ['' for x in data['FC_70_51']['data_type']['attributes']],
+    #                    "Trend": ['' for x in data['FC_70_51']['data_type']['attributes']]})
+    # print(df.head(5))
+    # df.to_excel('P_AOut attributes.xlsx')
+
+    data = data
+    scada_tags()
 
 
 if __name__ == '__main__':
