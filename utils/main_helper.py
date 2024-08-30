9b89ea081e8ad4ecb8d8ed66f9bc63b81a6738d5
Ciro Molina <cmolina@itgtec.com>
2024-08-30 15:34:03 -0400

    added scada_tag_groups method to generate csv files to import instances in iFix

--- utils/main_helper.py
+++ utils/main_helper.py
@@ -179,31 +179,44 @@ def scada_tags():
     # print(df)
 
     '''#AssetHeader,Asset Name,Description,Asset Type Name,Parent Asset Name,Asset Type Template,'''
-    # for instance in instances:
-    #     new_row = df.loc[5]
-    #     new_row['Asset Name'] = instance
-    #     df.loc[len(df)] = new_row
-    # df.to_csv('AssetHeader P_Motor.csv', index=False)
+    for instance in instances:
+        new_row = df.loc[5]
+        new_row['Asset Name'] = instance
+        df.loc[len(df)] = new_row
+    df.to_csv('AssetHeader P_Motor.csv', index=False)
 
     '''#AssetSubstituteParameters,ParameterName,AssetName,AssetTemplate,AssetType,ParamterValue,'''
-    # for instance in instances:
-    #     new_row = df.loc[2]
-    #     new_row['AssetName'] = new_row['ParamterValue'] = instance
-    #     df.loc[len(df)] = new_row
-    # df.to_csv('AssetSubstituteParameters P_ValveSO.csv', index=False)
+    for instance in instances:
+        new_row = df.loc[2]
+        new_row['AssetName'] = new_row['ParamterValue'] = instance
+        df.loc[len(df)] = new_row
+    df.to_csv('AssetSubstituteParameters P_ValveSO.csv', index=False)
 
     '''#TagSourceHeader,Parent Asset Name,Property Name,RealtimeServerAlias,RealtimeDataSourceName (iFix tag Id),HistoricalServerAlias,HistoricalDataSourceName,'''
-    # for instance in instances:
-    #     rows_to_copy = df[df['Parent Asset Name'] == 'Test_P_ValveSO'].copy()
-    #     rows_to_copy['Parent Asset Name'] = rows_to_copy['Parent Asset Name'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
-    #     rows_to_copy['RealtimeDataSourceName (iFix tag Id)'] = rows_to_copy['RealtimeDataSourceName (iFix tag Id)'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
-    #     rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(lambda x: str(x))
-    #     rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
-    #     df = pd.concat([df, rows_to_copy], ignore_index=True)
-    #
-    # df.to_csv('TagSourceHeader P_ValveSO.csv', index=False)
+    for instance in instances:
+        rows_to_copy = df[df['Parent Asset Name'] == 'Test_P_ValveSO'].copy()
+        rows_to_copy['Parent Asset Name'] = rows_to_copy['Parent Asset Name'].apply(
+            lambda x: x.replace('Test_P_ValveSO', instance))
+        rows_to_copy['RealtimeDataSourceName (iFix tag Id)'] = rows_to_copy[
+            'RealtimeDataSourceName (iFix tag Id)'].apply(lambda x: x.replace('Test_P_ValveSO', instance))
+        rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(lambda x: str(x))
+        rows_to_copy['HistoricalDataSourceName'] = rows_to_copy['HistoricalDataSourceName'].apply(
+            lambda x: x.replace('Test_P_ValveSO', instance))
+        df = pd.concat([df, rows_to_copy], ignore_index=True)
+
+    df.to_csv('TagSourceHeader P_ValveSO.csv', index=False)
 
     # print(df.shape)
+
+
+def scada_tag_groups():
+    instances = get_instances(file_path='tag-list-2024-08-09.json', data_type='P_AOut')
+    # print(instances)
+
+    for instance in instances:
+        with open(f'TGD_{instance}.csv', 'w') as file:
+            file.write(f'"1"\n')
+            file.write(f'"EquipmentName","{instance}",""')
 
 
 def main():
@@ -250,9 +263,8 @@ def main():
     # print(df.head(5))
     # df.to_excel('P_AOut attributes.xlsx')
 
-    data = data
-    scada_tags()
-
 
 if __name__ == '__main__':
     main()
+    scada_tags()
+    scada_tag_groups()
